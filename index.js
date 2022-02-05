2f2f615f41b4d8c98a7d8e632a987fc18a51fe07
moses_ultimatech <v.nikolaev@iwis.io>
2022-02-05 20:12:12 +0200

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({nevid: 'чорна велика криса'});
+    res.send({
+        nevid: 'чорна велика криса',
+        chomki4: 'дворф форт буаяр тебе жде'    
+    });
 })
 
 const PORT = process.env.PORT || 5000;
