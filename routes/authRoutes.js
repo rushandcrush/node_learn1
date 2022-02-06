3e8015ea96cf7a2d178f1ec74c7adc571c719679
moses_ultimatech <v.nikolaev@iwis.io>
2022-02-07 00:52:43 +0200

    add google/facebook passport nodejs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,19 @@
 const passport = require('passport');
 
 module.exports = app => {
+    
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }))
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope: [ 'email', 'user_location' ]
+    }))
+    
     app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get('/auth/facebook/callback', passport.authenticate('facebook'))
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user)
+    });
+
 }
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null, user)
+        })
+})
 
 passport.use(
     new GoogleStrategy(
@@ -9,10 +24,47 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         }, (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken: ', accessToken)
-            console.log('refreshToken: ', refreshToken)
-            console.log('profile: ', profile)
-            console.log('done: ', done)
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        done(null, existingUser)
+                    } else {
+                        new User({
+                            googleId: profile.id
+                        })
+                        .save()
+                        .then(() => {
+                            user => done(null, user)
+                        })
+                    }
+                })
+        }
+    ),
+    
+);
+
+passport.use(
+    new FacebookStrategy(
+        {
+          clientID: keys.facebookClientID,
+          clientSecret: keys.facebookClientSecret,
+          callbackURL: '/auth/facebook/callback',
+        }, (accessToken, refreshToken, profile, done) => {
+            console.log(profile)
+            User.findOne({ facebookId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        done(null, existingUser)
+                    } else {
+                        new User({
+                            facebookId: profile.id
+                        })
+                        .save()
+                        .then(() => {
+                            user => done(null, user)
+                        })
+                    }
+            })
         }
     )
-);
+)
